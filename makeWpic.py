0bdcadf2bba3bf376392febb54cbd1f55b9401a3
Brendan Huffman <bh247484@muhlenberg.edu>
2014-10-02 10:48:06 -0400

    updated code and new picture
    ....

--- makeWpic.py
+++ makeWpic.py
@@ -1,2 +1,23 @@
 def makeWpic():
-  pass
+  fname=pickAFile()
+  pic=makePicture(fname)
+  wPic=makeEmptyPicture(getwidth(pic),getHeight(pic))
+  copyInto(pic,wPic,0,0)
+  show(wPic)
+  
+
+def grayscale(pic)
+  for col in range(getWidth(pic))
+    for row in range(getHeight(pic))
+      p=getPixel(pic,col,row)
+      np=getPixel(npic,col,row)
+
+def shrink(pic):
+  newPic=makeEmptyPicture(getWidth(pic)/2, getHeight(pic)/2)
+  for col in range(0,getWidth(pic),2):
+  print getWidth(pic),getWidth(newPic),getHeight(pic),getHeight(newPic)
+    for row in range(0,getHeight(pic),2):
+      ps=getPixel(pic,col,row)
+      pd=getPixel(newPic,col/2,row/2)
+      setColor(pd,getColor(ps))
+  return newPic
